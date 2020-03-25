f4e8878dedacaadbba0a70fdc3875f539ab9dd27
mcprog <3968713+mcprog@users.noreply.github.com>
2020-03-24 18:34:14 -0700

    added depth restrival to cates

--- angular-ethic-cert/src/app/categories/categories.component.ts
+++ angular-ethic-cert/src/app/categories/categories.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Tag } from '../taglist/taglist.component';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export interface Category extends Data {
   name: string;
   description: string;
-  tags: Tag[];
+  tags: any[];
 }
 
 const tempCategories: Category[] = [
@@ -33,9 +34,32 @@ export class CategoriesComponent implements OnInit {
   categories: Observable<Category[]>;
   private categoryCollection: AngularFirestoreCollection<Category>;
 
+  private lookupTags(data: Category) {
+    for (let i = 0; i < data.tags.length; ++i) {
+      data.tags[i].get().then(snap => {
+        data.tags[i] = snap.get('name');
+      });
+    } 
+    
+  }
+
+  noTags(tags: string[]): string {
+    if (tags.length == 0) {
+      return "No tags";
+    }
+    return "";
+  }
+
   constructor(private db : AngularFirestore) {
     this.categoryCollection = db.collection<Category>('categories');
-    this.categories = this.categoryCollection.valueChanges();
+    this.categories = this.categoryCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Category;
+        //this.lookupVendor(data);
+        this.lookupTags(data);
+        return data;
+      }))
+    );
   }
 
   getPath(docRef: DocumentReference): string {
